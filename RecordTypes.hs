aa415c224adf9028fc07a8da17aa5777dc2b38e7
wtjerry <wtjerry123@gmail.com>
2018-12-05 21:40:56 +0100

    added Record Type excample

---
+++ RecordTypes.hs
@@ -0,0 +1,14 @@
+module RecordTypes where
+
+-- use either one of those functions to access the value
+data Person = Person { firstName :: String
+                     , lastName :: String
+                     , age :: Int
+                     , height :: Float
+                     , phoneNumber :: String
+                     , iceCreateFlavor :: String
+                     } deriving (Show)
+
+demoPerson = Person "hans" "muster" 42 180 "123 456" "strawberry"
+
+demoPerson2 = Person { age=99, iceCreateFlavor="chocolate", firstName="kevin", lastName="white", height=160, phoneNumber="999 333" }
