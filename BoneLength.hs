5b0e7005b17ddf641e2c155d1b337f5dda6ae678
wtjerry <wtjerry123@gmail.com>
2018-12-12 14:11:40 +0100

    derived types form Show and added function type to boneLenth

--- BoneLength.hs
+++ BoneLength.hs
@@ -1,7 +1,7 @@
 module BoneLength(Human(..), Gender(..), boneLength) where
 
-data Gender = Female | Male
-data Human = Human Float Gender Float
+data Gender = Female | Male deriving (Show)
+data Human = Human Float Gender Float deriving (Show)
 
 decreaseForAge age
     | age > 30 = 0.06 * (age - 30)
@@ -10,6 +10,7 @@ decreaseForAge age
 maleSize tbl = 69.089 + 2.238 * tbl
 femaleSize tbl = 61.412 + 2.317 * tbl
 
+boneLength :: Human -> Float
 boneLength (Human age Male tbl) = (maleSize tbl) - (decreaseForAge age)
 boneLength (Human age Female tbl) = femaleSize tbl - (decreaseForAge age)
 
