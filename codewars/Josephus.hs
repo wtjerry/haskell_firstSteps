7698c7a315b943e729d1f653d315175f67442177
wtjerry <wtjerry123@gmail.com>
2019-06-06 12:05:52 +0200

    solve Josephus CodeWars challange

---
+++ codewars/Josephus.hs
@@ -0,0 +1,15 @@
+module Josephus where
+
+import Debug.Trace (trace)
+
+josephus [] _ = []
+josephus (x:[]) _ = [x]
+-- josephus xs k | trace ("--- " ++ show xs ++ " - " ++ show k ++ " ---") False = undefined
+josephus xs k = (xs !! index):(josephus (splitAtAndRemoveAndReorder xs index) k)
+    where index = if k > len then ((k-1) `mod` len) else (k-1)
+          len = length xs
+
+
+
+splitAtAndRemoveAndReorder xs k = (drop (k+1) xs) ++ (take k xs)
+
