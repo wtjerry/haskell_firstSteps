7e7a360338095a6c54a662bd937e7ed77179b575
wtjerry <wtjerry123@gmail.com>
2019-06-06 12:56:58 +0200

    solve Tribonacci CodeWars challange

---
+++ codewars/Tribonacci.hs
@@ -0,0 +1,12 @@
+module Tribonacci where
+
+tribonacci :: Num a => (a, a, a) -> Int -> [a]
+tribonacci (a, b, c) n = take n $ trib a b c
+
+add3 a b c = a + b + c
+
+trib a b c = a : b : c : zipWith3 add3 shifted0 shifted1 shifted2
+    where shifted0 = trib a b c
+          shifted1 = tail shifted0
+          shifted2 = tail shifted1
+
