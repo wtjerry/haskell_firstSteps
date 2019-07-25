b036176f7be0b905faa2e97336580e516ef8e51b
wtjerry <wtjerry123@gmail.com>
2019-07-25 11:56:17 +0200

    add hpffp Functor, Applicative and Monad instances for MaybeT

--- hpffp/src/MonadTransformerInstances.hs
+++ hpffp/src/MonadTransformerInstances.hs
@@ -4,7 +4,7 @@ module MonadTransformerInstances where
 
 newtype IdentityT m a =
   IdentityT
-    { runIdentity :: m a
+    { runIdentityT :: m a
     }
 
 instance (Functor f) => Functor (IdentityT f) where
@@ -21,4 +21,30 @@ instance (Monad m) => Monad (IdentityT m) where
   return :: a -> IdentityT m a
   return = pure
   (>>=) :: IdentityT m a -> (a -> IdentityT m b) -> IdentityT m b
-  (IdentityT ma) >>= f = IdentityT $  ma >>= runIdentity . f
+  (IdentityT ma) >>= f = IdentityT $ ma >>= runIdentityT . f
+
+newtype MaybeT m a =
+  MaybeT
+    { runMaybeT :: m (Maybe a)
+    }
+
+instance (Functor f) => Functor (MaybeT f) where
+  fmap :: (a -> b) -> MaybeT f a -> MaybeT f b
+  fmap g (MaybeT fma) = MaybeT $ (fmap . fmap) g fma
+
+instance (Applicative f) => Applicative (MaybeT f) where
+  pure :: a -> MaybeT f a
+  pure a = MaybeT $ (pure . pure) a
+  (<*>) :: MaybeT f (a -> b) -> MaybeT f a -> MaybeT f b
+  (MaybeT fmab) <*> (MaybeT fma) = MaybeT $ (<*>) <$> fmab <*> fma
+
+instance (Monad m) => Monad (MaybeT m) where
+  return :: a -> MaybeT m a
+  return = pure
+  (>>=) :: MaybeT m a -> (a -> MaybeT m b) -> MaybeT m b
+  (MaybeT mma) >>= f =
+    MaybeT $ do
+      maybeA <- mma
+      case maybeA of
+        Nothing -> return Nothing
+        Just a -> (runMaybeT . f) a
